8e7c3f4daa1937e43b2db7ad72b50893207a5392
rieckypoerwadiredja <rieckysyarief@gmail.com>
2026-03-25 17:27:17 +0700

    FIX: prisma build2

--- app/utils/prisma.js
+++ app/utils/prisma.js
@@ -1,14 +1,29 @@
-import "dotenv/config";
+// import "dotenv/config";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@/generated/prisma/client";
+
+// const connectionString = `${process.env.DATABASE_URL}`;
+
+// if (!connectionString) {
+//   throw new Error("DATABASE_URL is not defined in the .env file");
+// }
+
+// const adapter = new PrismaPg({ connectionString });
+// const prisma = new PrismaClient({ adapter });
+
+// export { prisma };
+
+import { Pool } from "pg";
+import { attachDatabasePool } from "@vercel/functions";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined in the .env file");
-}
+attachDatabasePool(pool);
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  adapter: new PrismaPg(pool),
+});
 
 export { prisma };
